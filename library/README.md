0a2b31314c24aa3f39054143210086d944166a44
Jan Redmann <janredmann1@gmail.com>
2020-11-16 15:02:51 +0100

    add cr from crud

--- library/README.md
+++ library/README.md
@@ -1,12 +1,13 @@
 Seed the database with the json data
 
-When i visit the URL /books i want to see a list of all my book titles from the database
+When i visit the URL /books i want to see a list of all my book titles from the database ✅
 
-When i click on a book title from the list i want to see all the details from the book
+When i click on a book title from the list i want to see all the details from the book ✅
 
-When i click on a link 'add a book' under the books list i am taken to a form to add a book
+When i click on a link 'add a book' under the books list i am taken to a form to add a book ✅
 
 When i click on Add i want to be taken to the detail view of the added book
+and the book should be added to the database ✅
 
 When i click on a link 'edit' under the book details i want to be taken to a form where i can edit the book.
 
--- library/routes/books.js
+++ library/routes/books.js
@@ -6,14 +6,49 @@ router.get('/books', (req, res) => {
   console.log('books');
   // get all the books from the database
   Book.find().then(books => {
-    // to check the data:
-    // console.log(books);
-    // res.send(books);
+    // render a books view to display them
     res.render('books', { booksList: books })
   }).catch(err => {
     console.log(err);
   })
-  // render a books view to display them
 });
+
+router.get('/books/add', (req, res) => {
+  res.render('bookForm');
+})
+
+router.get('/books/:id', (req, res) => {
+  const bookId = req.params.id;
+  // find the book with that id
+  Book.findById(bookId)
+    .then(book => {
+      // render the view
+      console.log(book);
+      res.render('bookDetails', { bookDetails: book })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
+
+router.post('/books', (req, res) => {
+  // const title = req.body.title;
+  // const author = req.body.author;
+  // const description = req.body.description;
+  // const rating = req.body.rating;
+  const { title, author, description, rating } = req.body;
+  console.log(title, author, description, rating);
+  // add the book to the database
+  Book.create({
+    title,
+    author,
+    description,
+    rating
+  })
+    .then(book => {
+      console.log(`${book.title} was added to the database`);
+      res.redirect(`/books/${book._id}`)
+    })
+})
 
 module.exports = router;
