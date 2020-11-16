b870fcc6a4fb36a6ff3bb77ce4447340cf8c1d01
Jan Redmann <janredmann1@gmail.com>
2020-11-16 11:45:23 +0100

    run irongenerate library

--- express-post/app.js
+++ express-post/app.js
@@ -6,8 +6,25 @@ app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: false }));
+let accessCount = 0;
+// middleware
+const count = () => {
+    return (req, res, next) => {
+        // console.log('hello from middleware');
+        accessCount++;
+        console.log(accessCount);
+        next();
+    }
+}
+
+// to add the middleware globally - for every request
+app.use(count());
 
 app.get('/', (req, res) => {
+    res.render('form');
+});
+
+app.get('/another-route', (req, res) => {
     res.render('form');
 });
 
